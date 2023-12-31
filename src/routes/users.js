d87f6910d9f76b8341022f2d21dc5a301265130f
ridhimanwazir <ridhimansingh@gmail.com>
2023-12-31 16:11:53 +0530

    removed redundant files

--- src/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Define routes for users
-router.get('/', (req, res) => {
-  res.send('List of users');
-});
-
-module.exports = router;
